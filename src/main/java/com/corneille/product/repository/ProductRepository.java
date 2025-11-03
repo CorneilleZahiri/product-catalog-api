68072327da0501baaf7ab92db3fffcc260d1658e
Corneille <cgbeaga@gmail.com>
2025-11-03 12:47:56 +0100

    Liste des produits avec leurs catégories

--- src/main/java/com/corneille/product/repository/ProductRepository.java
+++ src/main/java/com/corneille/product/repository/ProductRepository.java
@@ -1,8 +1,16 @@
 package com.corneille.product.repository;
 
 import com.corneille.product.entity.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
     boolean existsByNameIgnoreCase(String name);
+
+    @EntityGraph(attributePaths = "category")
+    @Query("select p from Product p")
+    Page<Product> productList(Pageable pageable);
 }
